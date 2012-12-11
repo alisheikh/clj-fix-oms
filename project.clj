1c579cc3839e89780b8d3ccb52f0713c684087d3
Nitin Punjabi <nptoronto@yahoo.com>
2012-12-11 13:49:29 +0100

    Implement adding an order

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject clj-fix-oms "0.1"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :native-path "/usr/local/lib"
+  :main clj-fix-oms.core)
--- src/clj_fix_oms/core.clj
+++ src/clj_fix_oms/core.clj
@@ -1,1 +1,16 @@
 (ns clj-fix-oms.core)
+
+(def active-orders (ref {}))
+(def canceled-orders (ref {}))
+(def positions (ref {}))
+
+(defn add-order [order]
+  (let [{:keys [symbol side price]} order]
+    (dosync
+      (if-let [orders-at-price (get-in @active-orders [symbol side price])]
+        (alter active-orders assoc-in [symbol side price]
+          (conj orders-at-price order))
+        (if-let [orders-on-side (get-in @active-orders [symbol side])]
+          (alter active-orders update-in [symbol side] into {price [order]})
+          (alter active-orders assoc-in [symbol side]
+                                        (sorted-map price [order])))))))
