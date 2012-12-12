a914fac22792e963b179c8a0672b85b8cb34ddce
Nitin Punjabi <nptoronto@yahoo.com>
2012-12-12 09:48:29 +0100

    Implement coordinated updates between open and closed orders

--- src/clj_fix_oms/core.clj
+++ src/clj_fix_oms/core.clj
@@ -5,43 +5,49 @@
 (def closed-orders (ref {}))
 (def positions (ref {}))
 
-(defn add-order [order]
+(defn add-order [order collection]
   (let [{:keys [symbol side price client-order-id]} order]
-    (dosync
-      (if-let [orders-at-price (get-in @open-orders [symbol side price])]
-        (alter open-orders update-in [symbol side price]
-          into [{:order-ids
-            (conj (:order-ids orders-at-price) client-order-id)}
-            {client-order-id order}])
-        (if (get-in @open-orders [symbol side])
-          (alter open-orders update-in [symbol side]
-            into {price {:order-ids (ordered-set client-order-id)
-                         client-order-id order}})
-          (alter open-orders assoc-in [symbol side]
-            (sorted-map price {:order-ids (ordered-set client-order-id)
-                               client-order-id order})))))))
+    (if-let [orders-at-price (get-in @collection [symbol side price])]
+      (alter collection update-in [symbol side price]
+        into [{:order-ids
+          (conj (:order-ids orders-at-price) client-order-id)}
+          {client-order-id order}])
+      (if (get-in @collection [symbol side])
+        (alter collection update-in [symbol side]
+          into {price {:order-ids (ordered-set client-order-id)
+                       client-order-id order}})
+        (alter collection assoc-in [symbol side]
+          (sorted-map price {:order-ids (ordered-set client-order-id)
+                             client-order-id order}))))))
 
 (defn locate-order-pos [order-id orders]
   (let [idx (.indexOf orders order-id)]
     (if (not= -1 idx) idx nil)))
 
 ; A removed order needs to be added to closed-orders.
-(defn remove-order [order]
+(defn remove-order [order collection]
   (let [{:keys [symbol side price client-order-id]} order]
-    (dosync
-      (if-let [active-order
-              (get-in @open-orders [symbol side price client-order-id])]
-        (let [orders (get-in @open-orders [symbol side price :order-ids])]
-          ; Remove the order id from the price's order list.
-          (alter open-orders update-in [symbol side price :order-ids]
-            disj client-order-id)
-          ; Remove the order from open-orders.
-          (alter open-orders update-in [symbol side price]
-            dissoc client-order-id)
-          ; Remove a price completely if there are no orders for it.
-          (if (empty? (get-in @open-orders [symbol side price :order-ids]))
-            (alter open-orders update-in [symbol side]
-              dissoc price)))))))
+    (if-let [active-order
+            (get-in @collection [symbol side price client-order-id])]
+      (let [orders (get-in @collection [symbol side price :order-ids])]
+        ; Remove the order id from the price's order list.
+        (alter collection update-in [symbol side price :order-ids]
+          disj client-order-id)
+        ; Remove the order from collection.
+        (alter collection update-in [symbol side price]
+          dissoc client-order-id)
+        ; Remove a price completely if there are no orders for it.
+        (if (empty? (get-in @collection [symbol side price :order-ids]))
+          (alter collection update-in [symbol side]
+            dissoc price))))))
+
+(defn new-order [order]
+  (dosync (add-order order open-orders)))
+
+(defn cancel-order [order]
+  (dosync
+    (remove-order order open-orders)
+    (add-order order closed-orders)))
 
 ; This needs to be cleaned-up and search closed-orders as well.
 (defn find-order [order-id]
