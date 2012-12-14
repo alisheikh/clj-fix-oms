489e8196b750c3b64a08236c3032cc1566464fb9
Nitin Punjabi <nptoronto@yahoo.com>
2012-12-14 10:53:51 +0100

    Update project.clj with github URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-fix-oms "0.1"
-  :description "FIXME: write description"
+(defproject clj-fix-oms "0.3"
+  :description "A lightweight order management system which can be used with clj-fix"
+  :url "https://github.com/nitinpunjabi/clj-fix-oms"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [edw/ordered "1.3.2"]]
-  :native-path "/usr/local/lib"
   :main clj-fix-oms.core)
